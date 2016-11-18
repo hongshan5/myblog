096ebd46155094a633abdeab0cc95964ae928e13
刘洪山 <hongshan.liu@msxf.com>
2016-11-18 16:49:56 +0800

    test for myself

---
+++ config/default.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hongshan.liu on 2016/11/18.
+ */
+module.exports = {
+    port: 3000,
+    session: {
+        secret: 'myblog',
+        key: 'myblog',
+        maxAge: 2592000000
+    },
+    mongodb: 'mongodb://localhost:3001/myblog'
+};
